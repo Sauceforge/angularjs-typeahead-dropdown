89f16681057301e47a8c1c4b68c31f141e93b319
Andrew Gray <andrew.paul.gray@gmail.com>
2015-03-09 20:26:20 +1100

    model fix?

--- src/angularjs-typeahead-dropdown.js
+++ src/angularjs-typeahead-dropdown.js
@@ -48,7 +48,7 @@ angular.module("typeaheadDropdown.tpl", [])
                                 a.mdl = [];
                             }
                             angular.extend(a.mdl, i);
-                            a.mdl = i[a.config.optionLabel];
+                            a.mdl = i;
                             if (a.events !== undefined) {
                                 a.events.onItemSelect(i);
                             }
