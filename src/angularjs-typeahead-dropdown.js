d17f4ba6b317c9f1bebd5ffaffbc8724ece25b35
Andrew Gray <andrew.paul.gray@gmail.com>
2015-03-09 18:20:28 +1100

    improvements

--- src/angularjs-typeahead-dropdown.js
+++ src/angularjs-typeahead-dropdown.js
@@ -1,9 +1,10 @@
+"use strict";
 angular.module("typeaheadDropdown.tpl", [])
         .run(["$templateCache",
             function (a) {
                 a.put("templates/typeaheadDropdown.tpl.html",
                     "<div>" +
-                    "<div ng-if=!options>Loading options...</div>" +
+                    //"<div ng-if=!options>Loading options...</div>" +
                     "<div ng-if=options class=dropdown dropdown>" +
                     "<div class=input-group>" +
                     "<input class=form-control placeholder=\"Select or type...\" ng-model=model[config.modelLabel] typeahead=\"op[config.optionLabel] for op in options | filter:$viewValue\" typeahead-editable=false typeahead-on-select=\"onSelect($item, $model, $label)\" ng-required=\"required\"> " +
@@ -27,27 +28,30 @@ angular.module("typeaheadDropdown.tpl", [])
         .directive("typeaheadDropdown", function() {
             return {
                 templateUrl: "templates/typeaheadDropdown.tpl.html",
-                scope: { model: "=ngModel", getOptions: "&options", config: "=?", events: "=", required: "=?ngRequired" },
+                scope: { model: "=ngModel", options: "=",  config: "=?", events: "=", required: "=?ngRequired" },
                 replace: !0,
                 link: function($scope, $element, $attrs) {
                     $scope.externalEvents = {
                         onItemSelect: angular.noop
                     };
+
                     angular.extend($scope.externalEvents, $scope.events || []);
                 },
 
-                controller: ["$scope", "$q",
-                    function(a, b) {
+                controller: ["$scope",
+                    function (a) {
                         a.config = angular.extend({ modelLabel: "name", optionLabel: "name" }, a.config),
-                            b.when(a.getOptions())
-                                .then(function(b) {
-                                    a.options = b;
-                                }),
-                            a.onSelect = function(b) {
-                                angular.extend(a.model, b);
-                                a.model[a.config.modelLabel] = b[a.config.optionLabel];
-                                a.events.onItemSelect(b);
+
+                        a.onSelect = function (i) {
+                            if (a.model === undefined) {
+                                a.model = [];
                             }
+                            angular.extend(a.model, i);
+                            a.model[a.config.modelLabel] = i[a.config.optionLabel];
+                            if (a.events !== undefined) {
+                                a.events.onItemSelect(i);
+                            }
+                        }
                     }
                 ]
             }
