2c9a6297d182d91c1e55911c51c5cf5904810e80
Andrew Gray <andrew.paul.gray@gmail.com>
2016-07-16 09:46:14 +1000

    Add id to input field

--- src/angularjs-typeahead-dropdown.js
+++ src/angularjs-typeahead-dropdown.js
@@ -7,7 +7,7 @@ angular.module("typeaheadDropdown.tpl", [])
                     //"<div ng-if=!options>Loading options...</div>" +
                     "    <div ng-if=options class=dropdown dropdown>" +
                     "        <div class=input-group>" +
-                    "            <input class=form-control placeholder=\"Select or type...\" ng-model=mdl[config.optionLabel] typeahead=\"op[config.optionLabel] for op in options | filter:$viewValue\" typeahead-editable=false typeahead-on-select=\"onSelect($item, $model, $label)\" ng-required=\"required\" ng-disabled=\"disabled\"> " +
+                    "            <input id=\"typeahead_dropdown\" class=form-control placeholder=\"Select or type...\" ng-model=mdl[config.optionLabel] typeahead=\"op[config.optionLabel] for op in options | filter:$viewValue\" typeahead-editable=false typeahead-on-select=\"onSelect($item, $model, $label)\" ng-required=\"required\" ng-disabled=\"disabled\"> " +
                     "            <span class=input-group-btn>" +
                     "                <button class=\"btn btn-default dropdown-toggle\" dropdown-toggle ng-disabled=\"disabled\">" +
                     "                    <span class=caret></span>" +
