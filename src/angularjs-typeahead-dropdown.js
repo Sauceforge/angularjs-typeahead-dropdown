a5d3e630d2e96875ffb4c1526c178e368397beb4
Andrew Gray <andrew.paul.gray@gmail.com>
2016-08-04 21:57:25 +1000

    add uib-

--- src/angularjs-typeahead-dropdown.js
+++ src/angularjs-typeahead-dropdown.js
@@ -5,7 +5,7 @@ angular.module("typeaheadDropdown.tpl", [])
                 a.put("templates/typeaheadDropdown.tpl.html",
                     "<div>" +
                     //"<div ng-if=!options>Loading options...</div>" +
-                    "    <div ng-if=options class=dropdown dropdown>" +
+                    "    <div ng-if=options class=uib-dropdown dropdown>" +
                     "        <div class=input-group>" +
                     "            <input id=\"typeahead_dropdown\" class=form-control placeholder=\"Select or type...\" ng-model=mdl[config.optionLabel] typeahead=\"op[config.optionLabel] for op in options | filter:$viewValue\" typeahead-editable=false typeahead-on-select=\"onSelect($item, $model, $label)\" ng-required=\"required\" ng-disabled=\"disabled\"> " +
                     "            <span class=input-group-btn>" +
@@ -14,7 +14,7 @@ angular.module("typeaheadDropdown.tpl", [])
                     "                </button>" +
                     "            </span>" +
                     "        </div>" +
-                    "        <ul class=dropdown-menu role=menu style=max-height:200px;overflow-y:auto>" +
+                    "        <ul class=uib-dropdown-menu role=menu style=max-height:200px;overflow-y:auto>" +
                     "            <li ng-repeat=\"op in options\">" +
                     "                <a href ng-click=onSelect(op)>{{op[config.optionLabel]}}</a>" +
                     "            </li>" +
